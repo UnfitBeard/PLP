a7f01dcf9ebf4cf0af3ec71b3762888a1cc07d2b
Mr.Nobody <njunge995@gmail.com>
2025-05-05 11:36:31 +0300

    The SQL Projects Both

--- Final SQL Project/Question2/databaseconn.js
+++ Final SQL Project/Question2/databaseconn.js
@@ -1,12 +1,12 @@
 import mysql from 'mysql2/promise'
 const connection = mysql.createPool({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
-    database: "yourdatabase",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "yourdatabase",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export default connection
